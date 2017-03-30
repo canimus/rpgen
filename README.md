f3d6a58b07506f66d95c370a931abb3b23b7dbb9
Herminio Vazquez <canimus@gmail.com>
2017-03-30 21:22:00 +0200

    Addition of comments to launch application

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ This is a small project that uses a key-value store in Redis to retrieve unique 
 ```ruby
 # To get the libraries
 bundle install
+bundle package
 ```
 
 ## Run
@@ -26,5 +27,30 @@ bundle exec ruby app.rb
 
 ## View
 ```ruby
+# Only on MacOS X
 open "http://localhost:4567/"
+```
+
+## Docker Execution
+If willing to run inside of the containers then first need to pull the right containers
+```sh
+# In your MacOS
+docker-machine create -d virtualbox --virtualbox-share-folder "$(PWD)":/sw/apps redis-server
+docker ssh redis-server
+
+# Getting containers inside of virtualbox
+docker pull redis
+docker pull ruby:2.3.3
+
+# Running the containers
+docker run -d -p 6379:6379 --name redis-srv redis
+docker run -it --rm --name randomperson --link redis-srv:redis-srv -p 4567:4567 -v "$PWD":/usr/src/myapp -w /usr/src/myapp ruby:2.3.
+3 bash -c "bundle install --local && bundle exec ruby app.rb"
+```
+
+## Inserting records
+The following commands are required to insert records
+```redis
+LPUSH people user:1
+
 ```
--- app.rb
+++ app.rb
@@ -1,30 +1,43 @@
 # Author: Herminio Vazquez
 # Date: March 21, 2017
-# Project: ING Mortgages /  Force Application
 # Location: Amsterdam, Netherlands
 # File: app.rb
 # Description: Web service to generate random people
 # =====================================================
 
+# Docker Machine
 # docker-machine create -d virtualbox redis-server
+# =====================================================
+
+# Docker
 # docker run -d --name redis-srv -p 6379:6369 redis
 # VBoxManage controlvm "redis-server" natpf1 "tcp-port6379,tcp,,6379,,6379";
 # VBoxManage controlvm "redis-server" natpf1 "tcp-port4567,tcp,,4567,,4567";
+# =====================================================
+
+
+# Redis Operations
+# docker exec -ti redis-srv bash
+# redis-cli
+# SADD people user:1
+# HMSET user:1 name "John Smith" sofinummer 10203040 initials "J.S" sex Male phone 666777999 email jsmith@test.com street "5th Avenue" house 110 city "New York" regio "New Jersey" postcode 5533TH
+# =====================================================
 
 require 'sinatra'
 require 'sinatra/json'
 require 'redis'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
+set :bind, '0.0.0.0'
 
 get '/' do
-  redis = Redis.new
+  redis = Redis.new(:host => "redis-srv")
   @people = redis.scard 'people'
   haml :index
 end
 
 get '/people' do
-  redis = Redis.new
+  redis = Redis.new(:host => "redis-srv")
   counter = redis.scard 'people'
   key = redis.spop 'people'
   person = redis.hgetall(key)
